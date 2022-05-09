d7a3372736243868ebfc339455ad1cd4ac11104c
Eptick <redzicleon@gmail.com>
2022-05-09 22:27:20 +0200

    adasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DotComponent } from './dot/dot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DotComponent
   ],
   imports: [
     BrowserModule
